69a18a6ce83ce2c529e990834ef7796c1c12b021
romanitalian <romanitalian.net@gmail.com>
2018-04-26 19:57:36 +0300

    make a "race" code example

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"time"
+	"fmt"
+)
+
+type intCounter int64
+
+func (c *intCounter) Add(x int64) {
+	*c++
+}
+func (c *intCounter) Value() int64 {
+	return int64(*c)
+}
+func main() {
+	counter := intCounter(0)
+	for i := 0; i < 100; i++ {
+		go func(no int) {
+			for i := 0; i < 10000; i++ {
+				counter.Add(1)
+			}
+		}(i)
+	}
+	time.Sleep(time.Second)
+	fmt.Println(counter.Value())
+}
